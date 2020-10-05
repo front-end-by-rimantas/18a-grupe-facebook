8f245016a960b36a1dcdb606f90a434873fde791
Rimantas <belauzas@yahoo.com>
2020-10-05 14:11:23 +0300

    Extra photos

--- js/renderContentGallery.js
+++ js/renderContentGallery.js
@@ -10,28 +10,25 @@ function renderContentGallery(photoArray) {
     const printablePhotosCount = photoArray.length > maxGallerySize ? maxGallerySize : photoArray.length;
 
     // generuojame tik reikiama kieki nuotrauku
-    let photosHTML = '';
-    for (let i = 0; i < printablePhotosCount; i++) {
-        photosHTML += `<img src="./img/posts/${photoArray[i]}" alt="User post gallery picture">`;
+    let extraHTML = '';
+    if (photoArray.length > maxGallerySize) {
+        extraHTML = `data-extra="${photoArray.length - maxGallerySize}"`;
     }
 
-    // senesnis variantas - geras, bet virsuje geriau
-    // for (let i = 0; i < photoArray.length; i++) {
-    //     if (i >= maxGallerySize) {
-    //         break;
-    //     }
-    //     photosHTML += `<img src="./img/posts/${photoArray[i]}" alt="User post gallery picture">`;
-    // }
-
-    // generuojame extra skaiciuka, jeigu reikia
-    let moreHTML = '';
-    if (photoArray.length > maxGallerySize) {
-        moreHTML = `<div class="more-photos">+${photoArray.length - maxGallerySize}</div>`;
+    let photosHTML = '';
+    for (let i = 0; i < printablePhotosCount; i++) {
+        if (i < printablePhotosCount - 1) {
+            photosHTML += `<img src="./img/posts/${photoArray[i]}"
+                                alt="User post gallery picture">`;
+        } else {
+            photosHTML += `<img src="./img/posts/${photoArray[i]}"
+                                alt="User post gallery picture"
+                                ${extraHTML}>`;
+        }
     }
 
     return `<div class="gallery gallery-${printablePhotosCount}">
                 ${photosHTML}
-                ${moreHTML}
             </div>`;
 }
 
