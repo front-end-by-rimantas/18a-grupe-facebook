d637ca11564a42597ef909067814950e4475bd25
Rimantas <belauzas@yahoo.com>
2020-10-06 11:02:21 +0300

    Last gallery photo pseudo element before

--- js/renderContentGallery.js
+++ js/renderContentGallery.js
@@ -6,24 +6,29 @@ function renderContentGallery(photoArray) {
     }
 
     // logika
-    const maxGallerySize = 3;
-    const printablePhotosCount = photoArray.length > maxGallerySize ? maxGallerySize : photoArray.length;
+    const maxGallerySize = 4;
+    const photosCount = photoArray.length;
+    const printablePhotosCount = photosCount > maxGallerySize ? maxGallerySize : photosCount;
 
     // generuojame tik reikiama kieki nuotrauku
     let extraHTML = '';
-    if (photoArray.length > maxGallerySize) {
-        extraHTML = `data-extra="${photoArray.length - maxGallerySize}"`;
+    if (photosCount > maxGallerySize) {
+        extraHTML = `data-extra="${photosCount - maxGallerySize}"`;
+        // extraHTML = `<div class="black-hover">+${photosCount - maxGallerySize}</div>`;
     }
 
     let photosHTML = '';
     for (let i = 0; i < printablePhotosCount; i++) {
         if (i < printablePhotosCount - 1) {
-            photosHTML += `<img src="./img/posts/${photoArray[i]}"
-                                alt="User post gallery picture">`;
+            photosHTML += `<div class="img">
+                                <img src="./img/posts/${photoArray[i]}"
+                                    alt="User post gallery picture">
+                            </div>`;
         } else {
-            photosHTML += `<img src="./img/posts/${photoArray[i]}"
-                                alt="User post gallery picture"
-                                ${extraHTML}>`;
+            photosHTML += `<div class="img" ${extraHTML}>
+                                <img src="./img/posts/${photoArray[i]}"
+                                    alt="User post gallery picture">
+                            </div>`;
         }
     }
 
